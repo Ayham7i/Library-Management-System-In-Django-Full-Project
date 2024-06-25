57ddbbc18fdfc5d2daaa9623aa206476df2bae58
Ayham7i <ayham1010song@gmail.com>
2024-06-25 21:05:48 +0300

    Add The Database and all of the necessary configuration

--- LMS_APP/models.py
+++ LMS_APP/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+
+class Book(models.Model):
+
+    status_book = [
+        ('Available','Available'),
+        ('Rental','Rental'),
+        ('Sold','Sold'),
+    ]
+    title = models.CharField(max_length=250)
+    author = models.CharField(max_length=250)
+    book_photo = models.ImageField(upload_to='photos/book_photo' , null=True , blank=True)
+    author_photo = models.ImageField(upload_to='photos/author_photo' , null=True , blank=True)
+    pages = models.IntegerField(null=True , blank=True)
+    price = models.DecimalField(max_digits=5,decimal_places=2 , null=True , blank=True)
+    rental_price_day = models.DecimalField(max_digits=5, decimal_places=2,null=True , blank=True)
+    rental_period = models.IntegerField(null=True,blank=True)
+    active = models.BooleanField(default=True)
+    status = models.CharField(max_length=50,choices= status_book , null=True , blank=True)
+    category = models.ForeignKey(Category,on_delete=models.PROTECT ,null=True , blank=True)
