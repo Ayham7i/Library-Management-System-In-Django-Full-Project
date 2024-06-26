3aa8373fcd13e9698aadd4f831a6baa34252f306
Ayham7i <ayham1010song@gmail.com>
2024-06-26 18:14:11 +0300

    Fixing The Database "object name"

--- LMS_APP/models.py
+++ LMS_APP/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
 
@@ -23,3 +25,6 @@ class Book(models.Model):
     active = models.BooleanField(default=True)
     status = models.CharField(max_length=50,choices= status_book , null=True , blank=True)
     category = models.ForeignKey(Category,on_delete=models.PROTECT ,null=True , blank=True)
+    
+    def __str__(self):
+        return self.title
