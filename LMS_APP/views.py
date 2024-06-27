e8ad4af4a40a7896dba7d4ad984af0279dce8ec4
Ayham7i <ayham1010song@gmail.com>
2024-06-27 12:09:16 +0300

    Fetch books from the database and display them on the site

--- LMS_APP/views.py
+++ LMS_APP/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request,'pages/index.html')
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request,'pages/index.html' ,context)
 
 def books(request):
     return render(request, 'pages/book.html')
