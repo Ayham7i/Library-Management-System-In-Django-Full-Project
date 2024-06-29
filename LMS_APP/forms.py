9d95fda8ae678af428b64bd3539dd7f66e914c17
Ayham7i <ayham1010song@gmail.com>
2024-06-29 22:50:52 +0300

    Fix the style of the book's input

--- LMS_APP/forms.py
+++ LMS_APP/forms.py
@@ -16,3 +16,17 @@ class BookForm(forms.ModelForm):
             'status',
             'category',
         ]
+        widgets = {
+
+            'title':forms.TextInput(attrs={'class':'form-control'}),
+            'author':forms.TextInput(attrs={'class':'form-control'}),
+            'book_photo':forms.FileInput(attrs={'class':'form-control'}),
+            'author_photo':forms.FileInput(attrs={'class':'form-control'}),
+            'pages':forms.NumberInput(attrs={'class':'form-control'}),
+            'price':forms.NumberInput(attrs={'class':'form-control'}),
+            'rental_price_day':forms.NumberInput(attrs={'class':'form-control'}),
+            'rental_period':forms.NumberInput(attrs={'class':'form-control'}),
+            'status':forms.Select(attrs={'class':'form-control'}),
+            'category':forms.Select(attrs={'class':'form-control'}),
+
+        }
