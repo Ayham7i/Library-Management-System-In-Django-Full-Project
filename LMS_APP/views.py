a00afe06ba0ad3deed3169ba7f818681d155bb53
Ayham7i <ayham1010song@gmail.com>
2024-06-29 23:15:47 +0300

    add the books from the pages "fix the form request and commit the dataase"

--- LMS_APP/views.py
+++ LMS_APP/views.py
@@ -5,6 +5,17 @@ from .forms import BookForm
 # Create your views here.
 
 def index(request):
+
+    if request.method == 'POST':
+        add_book  = BookForm(request.POST , request.FILES)
+        if add_book.is_valid:
+            add_book.save()
+    
+
+
+
+
+
     context = {
         'books': Book.objects.all(),
         'categories':Category.objects.all(),
