cdec84c8828c3e24d332f80cc740f5c069f64658
Ayham7i <ayham1010song@gmail.com>
2024-07-02 18:44:01 +0300

    delete the books from the index.html

--- LMS_APP/urls.py
+++ LMS_APP/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index,name="index" ),
     path('books',views.books,name='books'),
-    path('update/<int:id>',views.update , name='update')
+    path('update/<int:id>',views.update , name='update'),
+    path('delete/<int:id>',views.delete , name='delete')
 
 ]
--- LMS_APP/views.py
+++ LMS_APP/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render,get_object_or_404
 from .models import *
 from .forms import BookForm,CategoryForm
 
@@ -54,6 +54,15 @@ def update(request , id):
         }
 
     return render(request,'pages/update.html',context)
+
+
+
+def delete(request , id):
+    book_delete = get_object_or_404(Book,id=id)
+    if request.method == 'POST':
+        book_delete.delete()
+        return redirect('/')
+    return render(request,'pages/delete.html')
 
 
 
